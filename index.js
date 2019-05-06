7177cc6da5b5ca11fcc0103faa3e4ef0f6bc2d3b
Nikhil Kumar <nkumar0727@gmail.com>
2019-05-06 17:36:51 -0500

    MVP created; need to change color of navbar and make fix

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var typed = new Typed('.typed', {
+isDarkThemed = true;
+
+var typed = new Typed('#typed-name-span', {
   stringsElement: '#typed-name',
-  typeSpeed: 40,
-  backSpeed: 40
+  typeSpeed: 40
+});
+
+var typed2 = new Typed('#typed-exp-span', {
+  stringsElement: '#typed-exp',
+  typeSpeed: 30,
+  startDelay: 1000,
 });
